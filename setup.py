790b853c6cef81a26c11dfd869455f45d330afeb
Alex Ye <alexzye1@gmail.com>
2020-01-07 23:18:10 -0800

    Add comma

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
 	description="git user switcher",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
-	url="https://github.com/azye/gu"
+	url="https://github.com/azye/gu",
 	scripts=['./scripts/gu'],
 	packages=setuptools.find_packages(),
 	python_requires='>=3.6',
