5775ba54a73020df16e7cd44a3a0c0b2a77e26a3
Alex Ye <alexzye1@gmail.com>
2020-01-07 23:09:33 -0800

    Add license

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
 	name="gu",
 	version="0.0.1",
 	author="Alex Ye",
-	author_email="alex@example.com",
+	author_email="alexzye1@gmail.com",
 	description="git user switcher",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/azye/gu"
 	scripts=['./scripts/gu'],
 	packages=setuptools.find_packages(),
 	python_requires='>=3.6',
